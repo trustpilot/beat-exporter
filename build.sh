bb7af49ae03ae4ea2ccbb4d5857df9797dcbb13d
Audrius Karabanovas <auk@trustpilot.com>
2019-09-26 16:02:17 +0300

    Lets see, wip

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+GITVERSION=$(git describe --tags --always --long --dirty)
+GITBRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+GITREVISION=$(git log -1 --oneline | cut -d ' ' -f1)
+TIME=$(date +%FT%T%z)
+LDFLAGS="-s -X github.com/prometheus/common/version.Version=${GITVERSION} \
+-X github.com/prometheus/common/version.Revision=${GITREVISION} \
+-X github.com/prometheus/common/version.Branch=${GITBRANCH} \
+-X github.com/prometheus/common/version.BuildUser=root@localhost \
+-X github.com/prometheus/common/version.BuildDate=${TIME}"
+
+for OS in "darwin" "linux" "windows"
+do
+    for ARCH in "amd64" "386"
+    do 
+        echo "Building ${OS}/${ARCH}"
+        if [[ $OS == "windows" ]]; then
+            GO111MODULE=on CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -ldflags "${LDFLAGS}" -mod=vendor -tags 'netgo static_build' -a -o ".build/${OS}-${ARCH}/beat-exporter.exe"
+        else 
+            GO111MODULE=on CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -ldflags "${LDFLAGS}" -mod=vendor -tags 'netgo static_build' -a -o ".build/${OS}-${ARCH}/beat-exporter"
+        fi
+       
+    done
+done
+
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/trustpilot/beat-exporter
+
+go 1.13
+
+require (
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/prometheus/client_golang v1.1.0
+	github.com/prometheus/common v0.6.0
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/sys v0.0.0-20190804053845-51ab0e2deafa
+)
